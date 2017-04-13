7a89bf0becfc926f0d32df1dd187a3d5e70f9fc3
Jan Rembold <jan.rembold@virtual-identity.com>
2017-04-13 10:43:02 +0200

    initial commit with some more demo files

--- app/resources/react/tsxDemo.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM from "react-dom";
-import Hello from "./demo/Hello";
-
-$.fn.tsxDemo = function() {
-	ReactDOM.render(
-		<Hello name="Jeff Albertson" />,
-		this.get(0)
-	);
-};
